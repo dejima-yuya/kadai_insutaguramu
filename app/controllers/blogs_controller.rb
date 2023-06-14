d127afa760a8915ddb0f7d8ca22e17fb1f2eaa93
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-15 06:00:54 +0900

    blog投稿時に確認画面を挟むため

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -12,7 +12,11 @@ class BlogsController < ApplicationController
 
   # GET /blogs/new
   def new
-    @blog = Blog.new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
   end
 
   # GET /blogs/1/edit
@@ -55,6 +59,10 @@ class BlogsController < ApplicationController
       format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
       format.json { head :no_content }
     end
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
   end
 
   private
