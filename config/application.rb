be688b009a6c2d574b31cb4a4bd4647d4d472149
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-16 13:38:51 +0900

    タイムゾーンを設定

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module KadaiInsutaguramu
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
