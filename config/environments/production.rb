a4e275bc6ce533a090fe039de55f379d81c5786d
dejima-yuya <jimajapan33@yahoo.co.jp>
2023-06-16 11:17:20 +0900

    Herokuアプリを削除して、新規に作りなおしたHerokuアプリにデプロイするため

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'rocky-brushlands-08496.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'pacific-lake-62586.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
